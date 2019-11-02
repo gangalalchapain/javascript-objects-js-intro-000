1301d332b5f395124d9503f1e89af54dcef22c83
Ganga Lal Chapain <gangalalchapain@gmail.com>
2019-11-02 06:05:06 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -2,3 +2,9 @@ var playlist = {
   artistName : "The beatles",
   songTitle : "Lets go to sky"
 }
+
+
+function updatePlaylist(playlist, artistName, songTitle){
+  return Object.assign({}, obj, {[key] : value});
+  
+}
